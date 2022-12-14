3bca3cec79acaf72ad72bde3a487a6d7a708b9f5
Evgeniy <freemen0083@gmail.com>
2022-12-14 19:48:57 +0400

    #1

---
+++ Program copy.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Input number:");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input degree:");
+int dee = Convert.ToInt32(Console.ReadLine());
+int temp=1;
+int i=0;
+while (i<dee)
+{
+    temp=temp*numb;
+    i++;
+}
+Console.WriteLine("Number "+numb+" in "+dee+" degree="+temp);
