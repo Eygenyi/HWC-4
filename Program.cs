b55a7b5444501579de2bb9b377cf823564a92972
Evgeniy <freemen0083@gmail.com>
2022-12-14 19:46:09 +0400

    #2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input number:");
+int numb = Convert.ToInt32(Console.ReadLine());
+int temp=0;
+while (0<numb || 0>numb)
+{
+    temp=temp+numb%10;
+    numb=numb/10;
+}
+Console.WriteLine("Summ numbers is: " + Math.Abs(temp));
